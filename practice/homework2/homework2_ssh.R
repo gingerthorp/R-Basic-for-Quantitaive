0d5c11840514940e0a7c2dfbccd45bb8e3b01e59
shquant <qzpm5n@naver.com>
2021-04-08 09:56:30 +0900

    homework2 comprete

--- practice/homework2/homework2_ssh.R
+++ practice/homework2/homework2_ssh.R
@@ -3,95 +3,42 @@
 # name : 손승한
 # student number : 2021711370
 
-# Question 1 : 
-
-# --- Data Setting start ---
-
-# change directory
-getwd()
-setwd("/Users/thorp/R_script/R-Basic-for-Quantitaive/practice/homework2")
-
-# Input file : 401ksubs_description.txt
-desc = readLines("401ksubs_description.txt")
-
-# extracts column names from _description.txt
-count = 0
-column_name = NULL
-
-for(row in desc){
-  
-  if(row == ""){
-    count = count + 1
-  }
-  if(count == 1){
-    column_name <- append(column_name, unlist(strsplit(row, " ")))
-  }else if(count == 2){
-    break
-  }
-}
-
-column_name = column_name[!(column_name %in% c(""))] # vector에서 "" 값 삭제.
-
 # Install & Import package : readxl
 install.packages("readxl")
 library("readxl")
 
-# Input file : 401ksubs.xls
-data_set <- read_excel(
-  "401ksubs.xls", sheet = "401ksubs",
-  col_names = column_name, skip = 0 # (skip = 0)
-)
+# change directory
+setwd("/Users/thorp/R_script/R-Basic-for-Quantitaive/practice/homework2")
 
-# --- Data Setting end ---
+cols_name = c('e401k', 'inc', 'marr', 'male', 'age', 'fsize', 'nettfa', 'p401k', 'pira', 'incsq', 'agesq')
+
+data_set <- read_excel("401ksubs.xls", col_names = cols_name)
+# For this quest ion, use only the data for married people without children living at home ( marr =1, fsize =2)
+data_set <- subset(data_set, marr==1 & fsize==2)
 
 # Problem 1: How many married couples without children at home are in the data set?
-
-couples_without_children = subset(data_set, marr==1 & fsize==2)
-couples_without_children_count = nrow(couples_without_children)
+couples_without_children_count = nrow(data_set)
 sprintf("couples without children : %d", couples_without_children_count)
-
+# couples without children : 1494
 
 # Problem 2: Use OLS to estimate the model
-
-ols = lm(nettfa ~ I(inc + age), data = data_set)
-ols2 = lm(nettfa ~ inc, data = data_set)
-ols3 = lm(nettfa ~ age, data = data_set)
-
+ols = lm(nettfa ~ inc + age, data = data_set)
 summary(ols)
-summary(ols2)
-summary(ols3)
-par(mfrow = c(3,1))
-
-plot(nettfa ~ I(inc + age), data = data_set)
-abline(ols)
-plot(nettfa ~ inc, data = data_set)
-abline(ols2)
-plot(nettfa ~ age, data = data_set)
-abline(ols3)
-# coefficient estimates: 0.96722
-# Multiple R-squared: 0.1689
-# Interpret the slope coefficients: 
-# Are there any surprises in the slope estimates?
+# Intercept: -104.40455
+# Estimate-inc: 1.30783
+# Estimate-age: 1.66470
+# Multiple R-squared: 0.2021
+# Adjusted R-squared: 0.201
 
 # Problem 3: 
-# Is age statistically significant?
-# inc+age Multiple R-squared : 0.1689
-# inc     Multiple R-squared : 0.1418
-# 연령은 통계적으로 유의합니다. 왜냐하면, 나이를 더했을 경우 R-squared가 0.0271 증가하기 때문입니다.
+confint(ols, 'age', 0.95)
+#        2.5 %   97.5 %
+# age 1.334314 1.995085
 
-# Obtain the 95% confidence interval of 𝛽2
+# Problem 5:
+p_value_two = 2 * (1 - pt((1.66470-1)/0.16843, couples_without_children_count - 2))
+p_value_two # 8.300755e-05
 
-# Problem 4: 
-# Does the intercept from the regression in part (2) have an interesting meaning? Explain
-
-
-# Problem 5: Find the p value for the test H0: 𝛽2=1 against H1: 𝛽2≠1. Do you reject the null hypothesis at the 1% significant level?
-
-
-# Problem 6: Find the p value for the test H0: 𝛽2=1 against H1: 𝛽2>1. Do you reject the null hypothesis at the 1% significant level?
-
-
-intercept <- ols$coefficients[[1]]
-sprintf("Intercept of the equation : %f", intercept)
-coefficient <- ols$coefficients[[2]]
-sprintf("Coefficient of the equation : %f", coefficient)
+# Problem 6:
+p_value_one = (1 - pt((1.66470-1)/0.16843, couples_without_children_count - 2))
+p_value_one # 4.150377e-05
